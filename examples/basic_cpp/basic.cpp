7563f9b70693d09c775c878940102592887431e9
Igor Chorazewicz <igor.chorazewicz@intel.com>
2021-12-08 15:49:07 +0100

    XXX

--- examples/basic_cpp/basic.cpp
+++ examples/basic_cpp/basic.cpp
@@ -1,68 +1,26 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /* Copyright 2021, Intel Corporation */
 
-///////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Lewis Baker
-// Licensed under MIT license.
-///////////////////////////////////////////////////////////////////////////////
-
 /*
  * basic.cpp -- example showing miniasync integration with coroutines
  */
 
-#include <coroutine>
-#include <utility>
-#include <string>
+#include "libminiasync.h"
+
 #include <iostream>
 #include <queue>
 
-#include "libminiasync.h"
+#include "coroutine_helpers.hpp"
 
-/* Similar to https://github.com/lewissbaker/cppcoro/blob/master/include/cppcoro/task.hpp */
-struct task {
-  struct promise_type {
-      struct final_awaitable
-      {
-          bool await_ready() const noexcept { return false; }
-          void await_resume() noexcept {}
-
-          std::coroutine_handle<> await_suspend(std::coroutine_handle<task::promise_type> h) noexcept {
-			  auto &cont = h.promise().cont;
-              return cont ? cont : std::noop_coroutine();
-          }
-      };
-
-
-    task get_return_object() { return task{std::coroutine_handle<task::promise_type>::from_promise(*this)}; }
-    std::suspend_always initial_suspend() { return {}; }
-    auto final_suspend() noexcept { return final_awaitable{}; }
-    void return_void() {}
-    void unhandled_exception() {}
-
-    std::coroutine_handle<> cont;
-  };
-
-  void wait() {
-    h.resume();
-  }
-
-  bool await_ready() { return !h || h.done();}
-    std::coroutine_handle<> await_suspend(std::coroutine_handle<> aw) {
-      h.promise().cont = aw;
-      return h;
-    }
-    void await_resume() {}
-
-  std::coroutine_handle<task::promise_type> h;
-};
-
+/* Queue from which executor will take future */
+// XXX - could we put this into task? (to avoid global variable)
 std::queue<std::pair<std::vector<struct vdm_memcpy_future>, std::coroutine_handle<>>> futures;
 
 struct memcpy_task
 {
-	memcpy_task(void *dst, void *src, size_t n) {
-		auto *pthread_mover = vdm_new(vdm_descriptor_pthreads()); // XXX - lifetime
-		fut = vdm_memcpy(pthread_mover, dst, src, n);
+	memcpy_task(char *dst, char *src, size_t n) {
+		pthread_mover = std::shared_ptr<vdm>(vdm_new(vdm_descriptor_pthreads()), &vdm_delete);
+		fut = vdm_memcpy(pthread_mover.get(), (void*)dst, (void*)src, n);
 	}
 
 	bool await_ready()
@@ -75,17 +33,14 @@ struct memcpy_task
 		futures.emplace(std::vector<struct vdm_memcpy_future>{fut}, h);
 	}
 
-	// custom function
-	struct vdm_memcpy_future manual_await()
-	{
-		return fut;
-	}
-
 	void await_resume() {}
 
+	// XXX: change to unique_ptr after fixing when_all_awaitable
+	std::shared_ptr<vdm> pthread_mover;
 	struct vdm_memcpy_future fut;
 };
 
+/* Executor loop */
 void wait(struct runtime *r)
 {
 	while (futures.size()) {
@@ -101,34 +56,7 @@ void wait(struct runtime *r)
 	}
 }
 
-struct when_all
-{
-	// XXX - passing vector here leads to internal complier error...
-	when_all(memcpy_task a1, memcpy_task a2, memcpy_task a3): a1(a1), a2(a2), a3(a3)
-	{
-	}
-
-	bool await_ready()
-	{
-		return false;
-	}
-
-	void await_suspend(std::coroutine_handle<> h)
-	{
-		std::vector<struct vdm_memcpy_future> v;
-		v.emplace_back(a1.manual_await());
-		v.emplace_back(a2.manual_await());
-		v.emplace_back(a3.manual_await());
-
-		futures.emplace(v, h);
-	}
-
-	void await_resume() {}
-
-	memcpy_task a1, a2, a3;
-};
-
-task async_mempcy(void *dst, void *src, size_t n)
+task async_mempcy(char *dst, char *src, size_t n)
 {
 	std::cout << "Before memcpy" << std::endl;
 	co_await memcpy_task{dst, src, n/2};
@@ -144,9 +72,13 @@ task async_mempcy(void *dst, void *src, size_t n)
 	std::cout << "After 3 concurrent memcopies " << ((char*) dst) << std::endl;
 }
 
-task async_memcpy_print(std::string to_copy, char *buffer, const std::string &to_print)
+task async_memcpy_print(char *dst, char *src, size_t n, std::string to_print)
 {
-	co_await async_mempcy(reinterpret_cast<void*>(buffer), reinterpret_cast<void*>(to_copy.data()), to_copy.size());
+	auto a1 = async_mempcy(dst, src, n/2);
+	auto a2 = async_mempcy(dst + n/2, src + n/2, n - n/2);
+
+	co_await when_all(a1, a2);
+
 	std::cout << to_print << std::endl;
 }
 
@@ -161,7 +93,7 @@ main(int argc, char *argv[])
 
 	char buffer[buffer_size] = {0};
 	{
-		auto future = async_memcpy_print(to_copy, buffer, to_print);
+		auto future = async_memcpy_print(buffer, std::string(to_copy).data(), buffer_size, to_print);
 
 		std::cout << "inside main" << std::endl;
 
